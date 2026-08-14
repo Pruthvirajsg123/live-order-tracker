f096da870521abf921fc5eabf52bfd5464a85253
Pruthviraj Sg <pruthvirajsg123@gmail.com>
2026-08-14 18:01:11 +0530

    feat:initialize Express backend

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Live Order Tracker API is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
